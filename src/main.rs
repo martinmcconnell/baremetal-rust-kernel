2eeef341070d22bf5041bc6ca1b3ec9e82a47856
Martin McConnell <martinmcconnell@protonmail.com>
2023-08-08 15:08:42 +0100

    lll linker.ld, glabal_asm lib

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
-#! [no_std]
-#! [no_main]
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod boot {
+    use core::arch::global_asm;
+
+    global_asm!(
+        ".section .text._start"
+    );
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -8,5 +18,5 @@ pub extern "C" fn _start() -> ! {
 
 #[panic_handler]
 fn panic (_info: &PanicInfo) -> ! {
-    loop {} 
+    loop {}
 }
